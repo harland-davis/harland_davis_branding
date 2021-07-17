206fa0760574ace0ecc3a88a08d91bb237a89ee6
Kyle Davis <kyledavis@Kyles-iMac.local>
2021-07-17 15:40:22 -0600

    header added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <header className="Navbar">
+      <nav className="Navbar__navigation">
+        <div></div>
+        <div className="Navbar__logo"><a href="/">kyleDavis<span className="Navbar__logo-semicolon">;</span></a></div>
+        <div className="Navbar__spacer" />
+        <div className="Navbar__navigation-items">
+          <ul>
+            <li><a href="/">Projects</a></li>
+            <li><a href="/">About</a></li>
+            <li><a href="/">Github</a></li>
+            <li><a href="/">Github</a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
